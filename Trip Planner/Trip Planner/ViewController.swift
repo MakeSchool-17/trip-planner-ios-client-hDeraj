ce0ac53ca7dd73983293efd7ead1948a05705fff
Jared Harkins <jared.harkins@students.makeschool.com>
2015-10-12 12:57:12 -0700

    initial xcode project

---
+++ Trip Planner/Trip Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trip Planner
+//
+//  Created by Jared Harkins on 10/12/15.
+//  Copyright © 2015 Jared Harkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
